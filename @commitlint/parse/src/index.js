579b97792fd21777d577adf086bf4814a8eacde7
Elliot Chong <code@elliotjameschong.com>
2018-10-04 20:52:05 -0700

    fix: use changelogOpts as the base for parserOpts (#341)

--- @commitlint/parse/src/index.js
+++ @commitlint/parse/src/index.js
@@ -4,10 +4,10 @@ import defaultChangelogOpts from 'conventional-changelog-angular';
 export default parse;
 
 async function parse(message, parser = sync, parserOpts) {
-	if (!parserOpts || Object.keys(parserOpts || {}).length === 0) {
-		const changelogOpts = await defaultChangelogOpts;
-		parserOpts = changelogOpts.parserOpts;
-	}
+	const changelogOpts = await defaultChangelogOpts;
+	
+	// Use `changelogOpts` as the base options
+	parserOpts = Object.assign({}, changelogOpts, parserOpts);
 
 	const parsed = parser(message, parserOpts);
 	parsed.raw = message;
